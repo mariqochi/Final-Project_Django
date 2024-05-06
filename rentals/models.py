5138ce1b6001f7d6ea480894d57a463d1bab1c0b
mariqochi <mariqochi350@gmail.com>
2024-05-07 00:16:59 +0400

    Added a new model class to represent rental cars in the system

--- rentals/models.py
+++ rentals/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class Car(models.Model):
+    model_name = models.CharField(max_length=100)
+    manufacturing_year = models.PositiveIntegerField()
+    petrol_consumption = models.FloatField()
+    price_per_day = models.DecimalField(max_digits=10, decimal_places=2)
+    price_per_three_days = models.DecimalField(max_digits=10, decimal_places=2)
+    price_per_week = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.model_name
+
+class RentalAvailability(models.Model):
+    car = models.ForeignKey(Car, on_delete=models.CASCADE)
+    date = models.DateField()
+    is_available = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.car} - {self.date} - Available: {self.is_available}"
+
